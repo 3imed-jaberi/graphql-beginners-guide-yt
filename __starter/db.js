4f363ea3c70fd1ce4a91c1907b3448904bc23f1b
3imed-jaberi <imed_jebari@hotmail.fr>
2021-07-20 23:23:58 +0200

    Initialization 🚧

---
+++ __starter/db.js
@@ -0,0 +1,10 @@
+export default {
+  fruits: [
+    { name: 'Watermelon', logo: '🍉' },
+    { name: 'Strawberry', logo: '🍓' },
+    { name: 'Avocado', logo: '🥑' },
+    { name: 'Peache', logo: '🍑' },
+    { name: 'Banana', logo: '🍌' },
+    { name: 'Cherry', logo: '🍒' }
+  ]
+};
